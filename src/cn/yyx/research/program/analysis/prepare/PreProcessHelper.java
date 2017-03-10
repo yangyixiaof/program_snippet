5d0b5d458739cc41f379bea673f79e623924356e
DELL <DELL@DESKTOP-20DEPKI>
2017-03-10 22:16:17 +0800

    small change.

--- src/cn/yyx/research/program/analysis/prepare/PreProcessHelper.java
+++ src/cn/yyx/research/program/analysis/prepare/PreProcessHelper.java
@@ -1,50 +1,40 @@
 package cn.yyx.research.program.analysis.prepare;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.text.edits.TextEdit;
 
 import cn.yyx.research.program.eclipse.jdtutil.JDTParser;
+import cn.yyx.research.program.eclipse.searchutil.JavaSearch;
 
 public class PreProcessHelper {
 
 	public static void EliminateAllParameterizedType(JDTParser jdtparser) throws JavaModelException {
 		IJavaProject java_project = jdtparser.GetJavaProject();
 		IPackageFragmentRoot[] package_roots = java_project.getPackageFragmentRoots();
-
-		List<ICompilationUnit> units = new LinkedList<ICompilationUnit>();
-
-		for (IPackageFragmentRoot package_root : package_roots) {
-
-			// System.err.println("package_root:"+package_root);
-			IJavaElement[] fragments = package_root.getChildren();
-			for (int j = 0; j < fragments.length; j++) {
-				IPackageFragment fragment = (IPackageFragment) fragments[j];
-				IJavaElement[] javaElements = fragment.getChildren();
-				for (int k = 0; k < javaElements.length; k++) {
-					IJavaElement javaElement = javaElements[k];
-					if (javaElement.getElementType() == IJavaElement.COMPILATION_UNIT) {
-						units.add((ICompilationUnit) javaElement);
-					}
-				}
-			}
-		}
+		List<ICompilationUnit> units = JavaSearch.SearchForAllICompilationUnits(package_roots);
+		// System.err.println("unit_size:" + units.size());
 		for (final ICompilationUnit compilation_resource : units) {
 			TextEdit edit = PreProcessCompilationUnitHelper.EntirePreProcessCompilationUnit(compilation_resource,
 					jdtparser);
 			compilation_resource.applyTextEdit(edit, null);
-			CompilationUnit cu = compilation_resource.reconcile(ICompilationUnit.NO_AST, false, null, null);
-
+			CompilationUnit cu = compilation_resource.reconcile(ICompilationUnit.NO_AST, false, compilation_resource.getOwner(), null);
+			if (cu == null)
+			{
+				System.err.println("ModifiedCompilationUnit is null, something must be wrong!");
+				System.exit(1);
+			}
+			compilation_resource.save(null, true);
+			
 			// testing
-			System.out.println("CompilationUnit:"+cu);
+			// System.out.println("CompilationUnit:" + cu);
+			// testing
+			// System.out.println("ICompilationUnit:" + compilation_resource.getSource());
 		}
 	}
 
---
+++ src/cn/yyx/research/program/eclipse/searchutil/JavaSearch.java
@@ -0,0 +1,57 @@
+package cn.yyx.research.program.eclipse.searchutil;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+
+public class JavaSearch {
+	
+	public static List<ICompilationUnit> SearchForAllICompilationUnits(IPackageFragmentRoot[] package_roots) throws JavaModelException  {
+		List<ICompilationUnit> units = new LinkedList<ICompilationUnit>();
+		for (IPackageFragmentRoot package_root : package_roots) {
+			// System.err.println("package_root:"+package_root);
+			IJavaElement[] fragments = package_root.getChildren();
+			for (int j = 0; j < fragments.length; j++) {
+				IPackageFragment fragment = (IPackageFragment) fragments[j];
+				IJavaElement[] javaElements = fragment.getChildren();
+				for (int k = 0; k < javaElements.length; k++) {
+					IJavaElement javaElement = javaElements[k];
+					if (javaElement.getElementType() == IJavaElement.COMPILATION_UNIT) {
+						units.add((ICompilationUnit) javaElement);
+					}
+				}
+			}
+		}
+		return units;
+	}
+	
+	public static void SearchForWhereTheMethodIsInvoked() throws CoreException {
+		SearchPattern pattern = SearchPattern.createPattern("foo", IJavaSearchConstants.METHOD,
+				IJavaSearchConstants.REFERENCES, SearchPattern.R_EXACT_MATCH);
+		SearchRequestor requestor = new SearchRequestor() {
+			@Override
+			public void acceptSearchMatch(SearchMatch match) throws CoreException {
+				System.out.println(match.toString());
+			}
+		};
+		new SearchEngine().search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() },
+				SearchEngine.createWorkspaceScope(), requestor, null);
+	}
+	
+	public static void SearchForTheMethodDeclaration() throws CoreException {
+		
+	}
+
+}
