f35d518f30975d1383687252fc82d4d02a52a89a
DELL <DELL@DESKTOP-20DEPKI>
2017-04-25 09:51:36 +0800

    small change.

--- src/cn/yyx/research/program/ir/generation/IRGeneratorForOneProject.java
+++ src/cn/yyx/research/program/ir/generation/IRGeneratorForOneProject.java
@@ -2,6 +2,7 @@ package cn.yyx.research.program.ir.generation;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,6 +16,8 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.internal.corext.callhierarchy.CallHierarchy;
+import org.eclipse.jdt.internal.corext.callhierarchy.MethodWrapper;
 
 import cn.yyx.research.program.eclipse.jdtutil.JDTParser;
 import cn.yyx.research.program.eclipse.searchutil.JavaSearch;
@@ -27,8 +30,9 @@ import cn.yyx.research.program.ir.storage.node.highlevel.IRForOneClass;
 import cn.yyx.research.program.ir.storage.node.highlevel.IRForOneMethod;
 import cn.yyx.research.program.ir.storage.node.lowlevel.IRForOneInstruction;
 
+@SuppressWarnings("restriction")
 public class IRGeneratorForOneProject {
-	
+	// TODO two things: first, mark whether a method is constructor and its IType. second, test caller-roots method and JavaSearch Engine.
 	private IJavaProject java_project = null;
 	
 	private HashMap<IType, IRForOneClass> class_irs = new HashMap<IType, IRForOneClass>();
@@ -176,6 +180,29 @@ public class IRGeneratorForOneProject {
 			IRGeneratorForClassesInICompilationUnit irgfcicu = new IRGeneratorForClassesInICompilationUnit();
 			cu.accept(irgfcicu);
 		}
+	}
+	
+	public static List<IMethod> GenerateRootCallers()
+	{
+		// this must be invoked after GenerateForAllICompilationUnits(...).
+		Set<IMethod> mset = irgfop.method_irs.keySet();
+		IMember[] members = new IMember[mset.size()];
+		mset.toArray(members);
+		List<IMethod> caller_roots = new LinkedList<IMethod>();
+		CallHierarchy callHierarchy = CallHierarchy.getDefault();
+		MethodWrapper[] callers = callHierarchy.getCallerRoots(members);
+		for (MethodWrapper mw : callers)
+		{
+			IMember im = mw.getMember();
+			if (!(im instanceof IMethod))
+			{
+				System.err.println("Strange! why not IMethod.");
+				System.exit(1);
+			}
+			IMethod imd = (IMethod) im;
+			caller_roots.add(imd);
+		}
+		return caller_roots;
 	}
 	
 //	private void SelfAddToMethodIR(IMethod it, IRForOneMethod irfocbu)
