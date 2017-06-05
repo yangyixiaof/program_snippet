f61d071aa76259cfc9c613bb7521cfb988b17f33
DELL <DELL@DESKTOP-20DEPKI>
2017-06-05 11:36:20 +0800

    small change.

--- src/cn/yyx/research/program/ir/visual/dot/debug/DebugShowDotPic.java
+++ src/cn/yyx/research/program/ir/visual/dot/debug/DebugShowDotPic.java
@@ -9,11 +9,13 @@ import cn.yyx.research.program.ir.visual.meta.DotMeta;
 
 public class DebugShowDotPic {
 	
+	private static int debug_idx = 0;
+	
 	public static void ShowPicForTrace(FullTrace ft) {
 		FileUtil.EnsureDirectoryExist(DotMeta.DebugDotDir);
 		FileUtil.EnsureDirectoryExist(DotMeta.DebugPicDir);
 		if (IRControlMeta.deep_debug) {
-			CommonDotGenerator cdg = new CommonDotGenerator(ft.GetRootsForVisual(), ft, DotMeta.DebugDotDir + "/" + "Debug" + ".dot", ft.GetDescription());
+			CommonDotGenerator cdg = new CommonDotGenerator(ft.GetRootsForVisual(), ft, DotMeta.DebugDotDir + "/" + "Debug" + (++debug_idx) + ".dot", ft.GetDescription());
 			cdg.GenerateDot();
 			DotView.HandleAllDotsInDirectory(DotMeta.DebugDotDir, DotMeta.DebugPicDir);
 		}
