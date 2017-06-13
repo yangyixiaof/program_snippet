4adebfa4cb65d8cc9a166f33e08a0707c97bdb53
DELL <DELL@DESKTOP-20DEPKI>
2017-06-13 23:09:46 +0800

    small change.

--- src/cn/yyx/research/program/snippet/bootstrap/SnippetExtractor.java
+++ src/cn/yyx/research/program/snippet/bootstrap/SnippetExtractor.java
@@ -29,60 +29,63 @@ import cn.yyx.research.program.systemutil.SystemUtil;
 import cn.yyx.research.test.TestJavaSearch;
 
 public class SnippetExtractor implements IApplication {
-	
-	public static IJavaProject LoadProjectAccordingToArgs(String[] args) throws Exception 
-	{
+
+	public static IJavaProject LoadProjectAccordingToArgs(String[] args) throws Exception {
 		if (args.length != 2) {
 			throw new WrongArgumentException();
 		}
 		DebugLogger.Log("Just for test, this is the args:", args);
-		
+
 		// load projects
-		ProjectInfo epi = new ProjectInfo(args[0], args[1]);//args[0]:no_use args[1]:D:/eclipse-workspace-pool/eclipse-rcp-neon-codecompletion/cn.yyx.research.program.snippet.extractor
+		ProjectInfo epi = new ProjectInfo(args[0], args[1]);// args[0]:no_use
+															// args[1]:D:/eclipse-workspace-pool/eclipse-rcp-neon-codecompletion/cn.yyx.research.program.snippet.extractor
 		IJavaProject jproj = AnalysisEnvironment.CreateAnalysisEnvironment(epi);
-		
+
 		return jproj;
 	}
-	
+
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		EnvironmentUtil.Clear();
-		IJavaProject java_project = LoadProjectAccordingToArgs((String[])context.getArguments().get(IApplicationContext.APPLICATION_ARGS));
+		IJavaProject java_project = LoadProjectAccordingToArgs(
+				(String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS));
 		try {
 			// DebugLogger.Log("Start is invoked!");
-			// SystemUtil.Delay(1000);	
+			// SystemUtil.Delay(1000);
 			// testing.
 			if (IRControlMeta.test) {
 				TestJavaSearch.TestInAll(java_project);
 			} else {
 				IRGeneratorForOneProject.GenerateForAllICompilationUnits(java_project);
 				// generate each local method.
-				GenerateDotForEachIRCodeInIRProject irproj_local_generation = new GenerateDotForEachIRCodeInIRProject(DotMeta.ProjectEachMethodDotDir, DotMeta.ProjectEachMethodPicDir);
+				GenerateDotForEachIRCodeInIRProject irproj_local_generation = new GenerateDotForEachIRCodeInIRProject(
+						DotMeta.ProjectEachMethodDotDir, DotMeta.ProjectEachMethodPicDir);
 				irproj_local_generation.GenerateDots();
-				
-				if (false)
-				{
-				// generate for each full trace.
-				IRGeneratorForOneProject irinstance = IRGeneratorForOneProject.GetInstance();
-				Set<IMethod> roots = EclipseSearchForICallGraph.GetRootCallEntries(irinstance.GetInverseCallGraph());
-				
-				// debugging.
-				DebugLogger.Log("root size:" + roots.size());
-				DebugLogger.Log("root imethods:" + roots);
-				
-				InvokeMethodSelector ims = new InvokeMethodSelector(roots);
-				ims.StartSelectMethodsProcess();
-				List<MethodSelection> method_selects = ims.GetMethodSelections();
-				List<FullTrace> ft_traces = new LinkedList<FullTrace>();
-				Iterator<MethodSelection> mitr = method_selects.iterator();
-				while (mitr.hasNext()) {
-					MethodSelection ms = mitr.next();
-					CodeOnOneTraceGenerator cootg = new CodeOnOneTraceGenerator(ms);
-					FullTrace ft = cootg.GetFullTrace();
-					ft_traces.add(ft);
-				}
-				GenerateDotForEachFullTrace full_trace_generator = new GenerateDotForEachFullTrace(DotMeta.FullTraceDotDir, DotMeta.FullTracePicDir, ft_traces);
-				full_trace_generator.GenerateDots();
+
+				if (true) {
+					// generate for each full trace.
+					IRGeneratorForOneProject irinstance = IRGeneratorForOneProject.GetInstance();
+					Set<IMethod> roots = EclipseSearchForICallGraph
+							.GetRootCallEntries(irinstance.GetInverseCallGraph());
+
+					// debugging.
+					DebugLogger.Log("root size:" + roots.size());
+					DebugLogger.Log("root imethods:" + roots);
+
+					InvokeMethodSelector ims = new InvokeMethodSelector(roots);
+					ims.StartSelectMethodsProcess();
+					List<MethodSelection> method_selects = ims.GetMethodSelections();
+					List<FullTrace> ft_traces = new LinkedList<FullTrace>();
+					Iterator<MethodSelection> mitr = method_selects.iterator();
+					while (mitr.hasNext()) {
+						MethodSelection ms = mitr.next();
+						CodeOnOneTraceGenerator cootg = new CodeOnOneTraceGenerator(ms);
+						FullTrace ft = cootg.GetFullTrace();
+						ft_traces.add(ft);
+					}
+					GenerateDotForEachFullTrace full_trace_generator = new GenerateDotForEachFullTrace(
+							DotMeta.FullTraceDotDir, DotMeta.FullTracePicDir, ft_traces);
+					full_trace_generator.GenerateDots();
 				}
 			}
 		} catch (Exception e) {
@@ -93,15 +96,15 @@ public class SnippetExtractor implements IApplication {
 		SystemUtil.Delay(1000);
 		return IApplication.EXIT_OK;
 	}
-	
+
 	@Override
 	public void stop() {
-//		DebugLogger.Log("Force Stop is invoked!");
-//		try {
-//			AnalysisEnvironment.DeleteAllAnalysisEnvironment();
-//		} catch (CoreException e) {
-//			e.printStackTrace();
-//		}
+		// DebugLogger.Log("Force Stop is invoked!");
+		// try {
+		// AnalysisEnvironment.DeleteAllAnalysisEnvironment();
+		// } catch (CoreException e) {
+		// e.printStackTrace();
+		// }
 	}
-	
+
 }
